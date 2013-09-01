05fa3d5ad6900be2164d593d94c2a725c36a503a
GeorgiYolovski <bluef0rever@abv.bg>
2013-09-01 13:18:22 +0300

    Changed property names to camelCase when sending to clients.

--- Chat.Services/App_Start/WebApiConfig.cs
+++ Chat.Services/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace Chat.Services
 {
@@ -9,6 +10,9 @@ namespace Chat.Services
     {
         public static void Register(HttpConfiguration config)
         {
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ContractResolver = 
+                new CamelCasePropertyNamesContractResolver();
+
             config.Routes.MapHttpRoute(
                    name: "ContactsApi",
                    routeTemplate: "api/contacts/{action}/{id}",
